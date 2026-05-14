7e9b8c8712a259afef32f8d1b87406219cfc120d
Monochromania <davidjiayang@gmail.com>
2026-05-14 18:41:28 -0400

    Fixed task 1 TODO and modified README

--- src/Polymorphism.java
+++ src/Polymorphism.java
@@ -8,12 +8,12 @@ public class Polymorphism {
      */
     public static String getStudentRole(Person p) {
         // TODO: right now for student, it is not returning the role to be student.
-        return p.role;
+        return p.getRole();
     }
 }
 
 class Person {
-    public final String role = "General";
+    private final String role = "General";
     private final String name;
 
     public Person(String name) {
@@ -23,14 +23,22 @@ class Person {
     public String getName() {
         return this.name;
     }
+
+    public String getRole(){
+        return this.role;
+    }
 }
 
 class Student extends Person {
-    public final String role = "Student";
+    private final String role = "Student";
     private final String studentId;
 
     public Student(String name, String studentId) {
         super(name);
         this.studentId = studentId;
+    }
+
+    public String getRole(){
+        return this.role;
     }
 }
